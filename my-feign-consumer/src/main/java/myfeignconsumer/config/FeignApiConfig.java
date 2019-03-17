8dcfce87dda5a5a97bfdee61adf1304bf4394975
LoranceChen <w68055010@qq.com>
2019-03-17 16:04:13 +0800

    refine

--- my-feign-consumer/src/main/java/myfeignconsumer/config/FeignApiConfig.java
+++ my-feign-consumer/src/main/java/myfeignconsumer/config/FeignApiConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-//@ComponentScan(basePackages = {"myfeignunion.api"})
 @Import({FeignApiConfig.MyfeignUnionAppApi.class})
 public class FeignApiConfig {
 
